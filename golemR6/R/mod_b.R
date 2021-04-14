85b6fad646bacf4cca2436434c5c4d1ae066a031
Colin Fay <contact@colinfay.me>
2021-04-14 20:22:37 +0200

    Output the id

--- golemR6/R/mod_b.R
+++ golemR6/R/mod_b.R
@@ -10,7 +10,8 @@
 mod_b_ui <- function(id){
   ns <- NS(id)
   tagList(
-    
+    h2("Module b"),
+   textOutput(ns("choice")) 
   )
 }
 
@@ -22,6 +23,11 @@ mod_b_server <- function(input, output, session, questionnaire_responses){
   on("printinfo",{
       print(questionnaire_responses$resp_id)
     })
+  
+  output$choice <- renderText({
+    watch("printinfo")
+    questionnaire_responses$resp_id
+  })
   
 }
 ## To be copied in the UI
